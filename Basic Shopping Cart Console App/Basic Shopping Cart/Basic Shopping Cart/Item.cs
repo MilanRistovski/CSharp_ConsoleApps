41eb1eb4f39775b7f1397c5ddd5dac7bafbb40cc
MilanRistovski <60365038+MilanRistovski@users.noreply.github.com>
2020-11-27 14:05:25 +0100

    Add files via upload

---
+++ Basic Shopping Cart Console App/Basic Shopping Cart/Basic Shopping Cart/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Shopping_Cart
+{
+    public class Item
+    {
+        private int ID;
+        public string Name;
+
+        public Item(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+}
---
+++ Basic Shopping Cart Console App/Basic Shopping Cart/Basic Shopping Cart/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Basic_Shopping_Cart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cart cart = new Cart();
+
+            bool stillAddingItems = true;
+
+            while (stillAddingItems)
+            {
+                Console.Clear();
+                Console.WriteLine("What would you like to do? ");
+                Console.WriteLine("Type 'add' to add an item.");
+                Console.WriteLine("Type 'remove' to add an item.");
+                Console.WriteLine("Type 'show' to show your list.");
+                Console.WriteLine("Type 'clear' to delete all items from list.");
+                Console.WriteLine("Type 'quit' to quit.");
+                string response = Console.ReadLine();
+                Console.Clear();
+                switch (response)
+                {
+                    case "add":
+                        {
+                            Console.Write("What item would you like to add? ");
+                            string whatTheUserTyped = Console.ReadLine();
+                            cart.AddToCart(whatTheUserTyped);
+                            break;
+                        }
+                    case "quit":
+                        {
+                            stillAddingItems = false;
+                            break;
+                        }
+                    case "show":
+                        {
+                            foreach (string itemName in cart.GetCartItemNames())
+                            {
+                                Console.WriteLine(itemName);
+                            }
+                            Console.WriteLine("Press enter to continue");
+                            Console.ReadLine();
+                            break;
+                        }
+                    case "clear":
+                        {
+                            cart.Clear();
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
